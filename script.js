3b73e232c0b7c1bbabd4795861c86b5551374887
Scotty <carrscot@gmail.com>
2022-09-30 16:04:55 +0200

    Create div for buttons in index.html. Create function to clear all divs named Reset in script.js. Create start button.

--- script.js
+++ script.js
@@ -1,8 +1,68 @@
 const container = document.querySelector('#container');
 
+
 for(let i = 0; i < 256; i++) {
-const box = document.createElement('div');
-    box.classList.add('box');
+    const box = document.createElement('div');
+        box.classList.add('box');
+    
+    const change = document.querySelector('.box');
+        box.addEventListener('mouseover', black);
+
+    function black(){
+        box.style.backgroundColor = 'black';
+    }
 
 container.appendChild(box)
 }
+
+function startState() {
+    
+const container = document.querySelector('#container');
+
+    for(let i = 0; i < 256; i++) {
+        const box = document.createElement('div');
+            box.classList.add('box');
+        
+        const change = document.querySelector('.box');
+            box.addEventListener('mouseover', black);
+    
+        function black(){
+            box.style.backgroundColor = 'black';
+        }
+    }
+
+container.appendChild(box)
+}
+
+function resetState(){
+    const old = document.querySelector('#container');
+    old.innerHTML = '';
+    }
+
+
+const options = document.querySelector('#options')
+
+const start = document.createElement('button');
+    start.classList.add('start');
+    start.textContent = 'Start!';
+
+options.appendChild(start);
+
+const reset = document.createElement('button');
+    reset.classList.add('reset');
+    reset.textContent = 'Reset';
+
+options.appendChild(reset);
+
+
+const resetClick = document.querySelector('.reset');
+reset.addEventListener('click', resetState);
+
+
+const startClick = document.querySelector('.start');
+start.addEventListener('click', startState);
+
+
+
+
+
